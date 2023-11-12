79b9ac6bc52d28a222c4f1170262b70980db92ba
dylanmc1ntee <dylangmcintee@gmail.com>
2023-11-11 23:00:00 -0500

    print hello

---
+++ SudokuGame.c
@@ -0,0 +1,80 @@
+//Dylan McIntee
+//Sudoku Game
+
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+void createPuzzle(int r, int c, int boxes [r][c]);
+void displayPuzzle(int r, int c, int boxes [r][c]);
+void populateBox();
+void playRound();
+int playAgain();
+int winOrLose();
+
+int main()
+{
+    int r = 9;
+
+    int c = 9;
+
+    int grid [9][9];
+
+    createPuzzle(r, c, grid);
+
+    displayPuzzle(r, c, grid);
+
+    //hello
+
+    return 0;
+}
+
+void createPuzzle(int r, int c, int boxes [r][c])
+{
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            boxes[i][j] = 1;
+        }
+    }
+}
+
+void displayPuzzle(int r, int c, int boxes [r][c])
+{
+    printf("-------------------------------------------------------\n");
+
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            printf("|  ?  ");
+        }
+
+        printf("|");
+
+        printf("\n");
+        
+        printf("-------------------------------------------------------\n");
+    }
+}
+
+void populateBox()
+{
+
+}
+
+void playRound()
+{
+
+}
+
+int playAgain()
+{
+    return 1;
+}
+
+int winOrLose()
+{
+    return 1;
+}
